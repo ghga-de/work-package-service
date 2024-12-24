3d363f82429094a1c09d664992c87f3d80ed97c4
TheByronHimes <TheByronHimes@gmail.com>
2024-12-24 16:28:11 +0000

    Add cache-control header to WOT endpoint

--- src/wps/adapters/inbound/fastapi_/routes.py
+++ src/wps/adapters/inbound/fastapi_/routes.py
@@ -19,6 +19,7 @@
 import logging
 
 from fastapi import APIRouter, HTTPException, status
+from fastapi.responses import JSONResponse
 
 from wps.adapters.inbound.fastapi_.auth import UserAuthContext, WorkPackageAccessToken
 from wps.adapters.inbound.fastapi_.dummies import WorkPackageRepositoryDummy
@@ -137,17 +138,21 @@ async def create_work_order_token(
     file_id: str,
     repository: WorkPackageRepositoryDummy,
     work_package_access_token: WorkPackageAccessToken,
-) -> str:
+) -> JSONResponse:
     """Get an encrypted work order token using a work package access token."""
     try:
         if not (work_package_id and file_id and work_package_access_token):
             raise repository.WorkPackageAccessError
-        return await repository.work_order_token(
+
+        wot = await repository.work_order_token(
             work_package_id=work_package_id,
             file_id=file_id,
             check_valid=True,
             work_package_access_token=work_package_access_token,
         )
+
+        cache_control_header = {"Cache-Control": "max-age=30"}
+        return JSONResponse(content=wot, status_code=201, headers=cache_control_header)
     except repository.WorkPackageAccessError as error:
         raise HTTPException(status_code=403, detail=str(error)) from error
 
