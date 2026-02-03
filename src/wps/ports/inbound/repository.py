49b97a33044eaed0d2c4f0138d39fe2936cfca46
Christoph Zwerschke <c.zwerschke@dkfz-heidelberg.de>
2026-02-03 12:30:27 +0100

    Wording
    Co-authored-by: Byron Himes <thebyronhimes@gmail.com>

--- src/wps/ports/inbound/repository.py
+++ src/wps/ports/inbound/repository.py
@@ -151,7 +151,7 @@ class WorkPackageRepositoryPort(ABC):
     async def delete_upload_box(self, box_id: UUID4) -> None:
         """Delete an upload box with the given ID.
 
-        If such a box does not exist, an UploadBoxNotFoundError will be raised.
+        If no such box exists, an UploadBoxNotFoundError will be raised.
         """
 
     @abstractmethod
